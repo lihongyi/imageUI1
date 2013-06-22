0ee5e8dca931376a0949e8552a563ea7847237cb
lihongyi <lihongyi77@hotmail.com>
2013-06-22 11:40:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageUI1::Application.config.secret_token = 'b87de60e322fc15516921d95a541ba37fa78e70d935eb783435dfe198627c401af4bc012aa3d755b12a1311f6b9ab0dde33c2d88a66af3a07c0ed89495cca514'
